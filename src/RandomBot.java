b1cd02477d3c680a8bdb2ba42061b6b9a4a4d709
Michael Utama <michaelu670@gmail.com>
2023-09-23 22:29:45 +0700

    perf: use interrupted() check instead of sleep(0)
    Use interrupted() check to increase performance and reliability. Crude test shows 7B iteration with interrupt check while sleep(0) only 6M (timeout=4s)

--- src/RandomBot.java
+++ src/RandomBot.java
@@ -3,7 +3,9 @@ public class RandomBot extends Bot {
     protected int[] search() throws Exception {
         for (int i = 0; i < 10; i++) {
             i--;
-            Thread.sleep(0);
+            if (Thread.interrupted()) {
+                throw new InterruptedException();
+            }
         }
         // create random move
         return new int[]{(int) (Math.random()*8), (int) (Math.random()*8)};
