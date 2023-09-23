4dc64758dd1ff8096a60e12f5b7497a7cf49f163
Michael Utama <michaelu670@gmail.com>
2023-09-23 21:55:05 +0700

    none: init classes

---
+++ src/Chromosome.java
@@ -0,0 +1,10 @@
+/**
+ * Chromosome used in GeneticAlgorithmBot
+ * Dependent on current state:
+ * board with R remaining moves have Chromosome length of R
+ *
+ *
+ */
+public class Chromosome {
+
+}
---
+++ src/GeneticAlgorithmBot.java
@@ -0,0 +1,11 @@
+public class GeneticAlgorithmBot extends Bot {
+    /**
+     * Use genetic minimax to get next move
+     * @return int[2] - next move
+     */
+    @Override
+    protected int[] search() {
+        return new int[0];
+    }
+
+}
