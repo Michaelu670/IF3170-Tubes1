668287aa427d449a43abbbb24a0f89a042683775
Michael Utama <michaelu670@gmail.com>
2023-09-23 22:17:48 +0700

    fix: stop search() on timeout

--- src/RandomBot.java
+++ src/RandomBot.java
@@ -1,6 +1,10 @@
 public class RandomBot extends Bot {
     @Override
-    protected int[] search() {
+    protected int[] search() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            i--;
+            Thread.sleep(0);
+        }
         // create random move
         return new int[]{(int) (Math.random()*8), (int) (Math.random()*8)};
     }
