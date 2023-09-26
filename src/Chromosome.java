89f148a59af0e972dba148895d1e90bc7f1b98a4
Michael Utama <michaelu670@gmail.com>
2023-09-26 13:56:12 +0700

    none: add Chromosome class

--- src/Chromosome.java
+++ src/Chromosome.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Chromosome used in GeneticAlgorithmBot
  * Dependent on current state:
@@ -6,5 +9,64 @@
  *
  */
 public class Chromosome {
+    private State initialState;
+    private ArrayList<Integer> genome;
+
+    public Chromosome(State initialState, ArrayList<Integer> genome) {
+        this.initialState = initialState;
+        this.genome = genome;
+    }
+
+    /**
+     * mutate a chromosome with certain rate.
+     * using swap as mutation
+     * @param rate - mutation rate
+     */
+    public void mutate(double rate) {
+        Random random = new Random();
+        if (random.nextDouble() > rate) {
+            return;
+        }
+
+        // if size < 2; no mutation possible
+        if (genome.size() < 2) {
+            return;
+        }
+
+        // pick two index to swap uniformly
+        int index1 = random.nextInt(genome.size());
+        int index2 = random.nextInt(genome.size() - 1);
+        if (index2 >= index1) {
+            index2++;
+        }
+
+        // swap the values on two index
+        int temp = genome.get(index1);
+        genome.set(index1, genome.get(index2));
+        genome.set(index2, temp);
+    }
+
+    /**
+     * calculate objective function for corresponding set of moves
+     * @return double - objective value
+     */
+    public double objectiveFunction() {
+        return toState().objectiveFunction();
+    }
+
+    public State toState() {
+        try {
+            State p = (State) initialState.clone();
+            for (int pos: genome) {
+                p = p.move(pos / 8, pos % 8);
+            }
+            return p;
+        }
+        catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
 }
---
+++ src/ChromosomeBuilder.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ChromosomeBuilder {
+    int turnsLeft;
+    State state;
+    public ChromosomeBuilder() {
+
+    }
+
+    public ChromosomeBuilder(int turnsLeft, State state) {
+        this.turnsLeft = turnsLeft;
+        this.state = state;
+    }
+
+    public void setTurnsLeft(int turnsLeft) {
+        this.turnsLeft = turnsLeft;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    /**
+     *
+     * @return a random valid chromosome
+     */
+    public Chromosome build() {
+        ArrayList<Integer> empty = new ArrayList<>();
+        for (int i = 0; i < State.BOARD_SIZE; i++) {
+            for (int j = 0; j < State.BOARD_SIZE; j++) {
+                if (state.getValue(i, j).isEmpty()) {
+                    empty.add(i * State.BOARD_SIZE + j);
+                }
+            }
+        }
+
+        Collections.shuffle(empty);
+        ArrayList<Integer> genome = new ArrayList<>();
+        for (int i = 0; i < turnsLeft; i++) {
+            genome.add(empty.get(i));
+        }
+        return new Chromosome(state, genome);
+    }
+
+
+}
