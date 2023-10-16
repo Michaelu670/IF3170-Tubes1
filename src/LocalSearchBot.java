b1eea78b03005a6b1eb1acd05b6d6386568e48e7
Genvictus <johannstarkmk42@gmail.com>
2023-10-16 20:46:37 +0700

    rfc: bot state representation now use String defining its marker instead of isX boolean

--- src/LocalSearchBot.java
+++ src/LocalSearchBot.java
@@ -1,36 +1,21 @@
 public class LocalSearchBot extends Bot{
-    public LocalSearchBot() {
-        this.isX = false;
-    }
-
-    public LocalSearchBot(boolean isX) {
-        this.isX = isX;
+    public LocalSearchBot(String player_marker) {
+        super(player_marker);
     }
 
     @Override
     protected int[] search() throws Exception {
         double maxVal = -MAX_OBJ_VAL;
-        double minVal = MAX_OBJ_VAL;
         int[] nextMove = new int[2];
         for (int i = 0; i < State.BOARD_SIZE ; i++) {
             for (int j = 0; j < State.BOARD_SIZE ; j++) {
                 try {
                     State nextState = this.state.move(i, j);
-                    if (isX) {
-                        double heuristicValue = nextState.objectiveFunctionHeuristic();
-                        if(heuristicValue < minVal) {
-                            nextMove[0] = i;
-                            nextMove[1] = j;
-                            minVal = heuristicValue;
-                        }
-                    }
-                    else {
-                        double heuristicValue = nextState.objectiveFunctionHeuristic();
-                        if(heuristicValue > maxVal) {
-                            nextMove[0] = i;
-                            nextMove[1] = j;
-                            maxVal = heuristicValue;
-                        }
+                    double heuristicValue = nextState.objectiveFunctionHeuristic(this.player_marker);
+                    if(heuristicValue >= maxVal) {
+                        nextMove[0] = i;
+                        nextMove[1] = j;
+                        maxVal = heuristicValue;
                     }
 
                 } catch (Exception e) {
